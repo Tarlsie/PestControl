88c75810fca05abeea8f3e9f43d8eda37a146f1e
danieltarlow <danieltarlow@gmail.com>
2015-02-11 02:02:55 +0200

    feb 10 after midnight Added POI files and cell convertion to string check string output

--- app/src/main/java/com/example/danielt/pestcontrol/ReadExcelFile.java
+++ app/src/main/java/com/example/danielt/pestcontrol/ReadExcelFile.java
@@ -6,7 +6,16 @@ import android.util.Log;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.util.Iterator;
+
 import org.apache.poi.*;
+import org.apache.poi.hssf.usermodel.HSSFCell;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.poifs.filesystem.POIFSFileSystem;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
 
 
 /**
@@ -17,7 +26,7 @@ public class ReadExcelFile extends Activity {
     public ReadExcelFile(){
 
     }
-
+        String result="";
     public static void readExcelFile(Context context, String fileName){
 
         if(!isExternalStorageAvailable()|| isExternalStorageReadOnly()){
@@ -31,9 +40,29 @@ public class ReadExcelFile extends Activity {
 
             POIFSFileSystem myFileSystem = new POIFileSystem(fis);
 
+            HSSFWorkbook workBook = new HSSFWorkbook(myFileSystem);
+
+            HSSFSheet sheet = workBook.getSheetAt(0);
+
+            Iterator<Row> rowIter = sheet.rowIterator();
+
+            while (rowIter.hasNext()){
+
+                HSSFRow row = (HSSFRow) rowIter.next();
+                Iterator<Cell> cellIter = row.cellIterator();
+
+                while (cellIter.hasNext()){
+                    HSSFCell cell = (HSSFCell) cellIter.next();
+                    result += cell.toString()+" ";
+                    Log.i("cell result", cell.toString());
+                }
+
+            }
+
 
         }
-        catch(){}
+        catch(Exception e){e.printStackTrace();}
+        return;
     }
 
 }
